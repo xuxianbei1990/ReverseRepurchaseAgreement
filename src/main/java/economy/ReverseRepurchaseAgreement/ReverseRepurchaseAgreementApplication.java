a681bdc636000375107970181dba25d4535dd907
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2022-08-31 16:05:39 +0800

    Initial commit

---
+++ src/main/java/economy/ReverseRepurchaseAgreement/ReverseRepurchaseAgreementApplication.java
@@ -0,0 +1,13 @@
+package economy.ReverseRepurchaseAgreement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseRepurchaseAgreementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseRepurchaseAgreementApplication.class, args);
+	}
+
+}
