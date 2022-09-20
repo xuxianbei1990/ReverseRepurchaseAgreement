311ac1a8ab5b01245af69de08da65505b0b48291
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2022-09-20 16:50:52 +0800

    增加低价概率

--- src/main/java/economy/reverse/repurchase/agreement/strategy/BankShareOutBonus.java
+++ src/main/java/economy/reverse/repurchase/agreement/strategy/BankShareOutBonus.java
@@ -32,9 +32,15 @@ public class BankShareOutBonus {
 
     private static List<ShareOutBonus> shareOutBonuses = new ArrayList<>();
 
-    private static String[] dates = {"2015-07-01", "2016-06-30", "2017-06-30", "2018-07-17", "2019-07-10", "2020-07-10", "2021-07-15", "2022-07-08"};
+    private static String[] dates =
+//    {"2015-07-03", "2016-07-13", "2017-06-14", "2018-07-12", "2019-07-12", "2020-07-10", "2021-07-13", "2022-07-15"}
+    {"2015-07-01", "2016-06-30", "2017-06-30", "2018-07-17", "2019-07-10", "2020-07-10", "2021-07-15", "2022-07-08"}
+            ;
 
-    private static double[] shares = {3.01, 2.74, 2.78, 2.91, 3.06, 3.2, 3.26, 3.64};
+    private static double[] shares =
+    {3.01, 2.74, 2.78, 2.91, 3.06, 3.2, 3.26, 3.64}
+//    {6.7, 6.90, 7.4, 8.4, 9.4, 12, 12.53, 15.22}
+    ;
 
     static {
         for (int i = 0; i < dates.length; i++) {
@@ -48,20 +54,15 @@ public class BankShareOutBonus {
 
 
     /**
-     * 以建设银行为例。高于6%就分批买入，低于4%分批卖出
+     * 以建设银行为例。高于6%就分批买入，低于3%分批卖出
      */
     public void execute() {
         List<Sh600036> sh600036s = parseData();
+        doExecute(sh600036s, 1);
 //        saveData(sh600036s);
-        doExecute(sh600036s, 3);
     }
 
-    public static void main(String[] args) {
-        BankShareOutBonus bankShareOutBonus = new BankShareOutBonus();
-        bankShareOutBonus.execute();
-    }
-
-    private void doExecute(List<Sh600036> sh600036s, Integer times) {
+    private void doExecute(List<Sh600036> sh600036s, int times) {
         BigDecimal init = BigDecimal.valueOf(100000);
         Stack<BigDecimal> stacks = new Stack<>();
         newValue(init, stacks, times);
@@ -69,10 +70,13 @@ public class BankShareOutBonus {
 
         List<Sh600036> newSh = sh600036s;
         BigDecimal count = BigDecimal.ZERO;
+
+        List<Sh600036> low = new ArrayList<>();
         for (Sh600036 sh : newSh) {
             BigDecimal share = sureDate(sh.getCreateDate());
             //分红除以股价
-            if (share.divide(sh.getOpen().multiply(BigDecimal.valueOf(10)), 2, RoundingMode.HALF_UP).compareTo(BigDecimal.valueOf(0.05)) >= 0) {
+            if (share.divide(sh.getOpen().multiply(BigDecimal.valueOf(10)), 2, RoundingMode.HALF_UP).compareTo(BigDecimal.valueOf(0.06)) >= 0) {
+                low.add(sh);
                 if (!stacks.isEmpty()) {
                     BigDecimal init1 = stacks.pop();
                     count = init1.divide(sh.getOpen(), 0, RoundingMode.UP);
@@ -101,9 +105,15 @@ public class BankShareOutBonus {
         if (count.compareTo(BigDecimal.ZERO) > 0) {
             sum = count.multiply(newSh.get(newSh.size() - 1).getOpen());
         }
+        System.out.println("概率" + BigDecimal.valueOf(low.size()).divide(BigDecimal.valueOf(sh600036s.size()), 5, BigDecimal.ROUND_HALF_UP));
         System.out.println(count);
         System.out.println(sum);
         System.out.println(sum.subtract(BigDecimal.valueOf(100000)).divide(BigDecimal.valueOf(100000).multiply(BigDecimal.valueOf(8)), 2, RoundingMode.UP));
+    }
+
+    public static void main(String[] args) {
+        BankShareOutBonus bankShareOutBonus = new BankShareOutBonus();
+        bankShareOutBonus.execute();
     }
 
     private void newValue(BigDecimal init, Stack<BigDecimal> stacks, Integer times) {
