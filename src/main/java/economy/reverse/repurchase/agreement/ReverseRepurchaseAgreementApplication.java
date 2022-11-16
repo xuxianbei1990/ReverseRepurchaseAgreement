106d4d06479c84ee286508130238d374c3bda1b7
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2022-11-16 11:12:46 +0800

    增加druid配置

--- src/main/java/economy/reverse/repurchase/agreement/ReverseRepurchaseAgreementApplication.java
+++ src/main/java/economy/reverse/repurchase/agreement/ReverseRepurchaseAgreementApplication.java
@@ -1,11 +1,13 @@
 package economy.reverse.repurchase.agreement;
 
+import college.myplugs.spring.MyMapperScans;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("economy.reverse.repurchase.agreement.dao")
 @SpringBootApplication
+@MyMapperScans
 public class ReverseRepurchaseAgreementApplication {
 
 	public static void main(String[] args) {
